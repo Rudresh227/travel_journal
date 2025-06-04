96306ebb0e1db40a87a3e1f262aa1533ff29cc4a
Rudresh <rudreshvasanth7@gmail.com>
2025-06-05 00:18:38 +0530

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Earth } from 'lucide-react'
+
+function Header(){
+    return(
+        <div className="nav-header">
+            <Earth color="#ffffff" />
+            <p>My Travel Journal</p>
+        </div>
+    )
+}
+
+export default Header;
